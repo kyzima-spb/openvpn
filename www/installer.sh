a7758590a5a04d436f26c04927e714ead5d2b13b
Kirill Vercetti <office@kyzima-spb.com>
2025-09-07 20:32:49 +0300

    Workflow for build image and helper files

--- www/installer.sh
+++ www/installer.sh
@@ -80,7 +80,7 @@ install_requirements() {
 install() {
   declare -n kwargs=$1
 	local name="${kwargs[name]}"
-	local image="${kwargs[image]:-https://kyzima-spb.github.io/openvpn/rootfs.tar.xz}"
+	local image="${kwargs[image]:-https://kyzima-spb.github.io/openvpn/images/trixie/rootfs.tar.xz}"
 	local port="${kwargs[port]:-$(get_free_port)}"
 	local public_key="${kwargs[public_key]:-0xA2AFF7EB363E6C8DD27655AD62CD962F89DDC0CD}"
 
@@ -292,7 +292,7 @@ main() {
   esac
 
   declare -A cli_options=(
-    [name]="p2p"
+    [name]="openvpn"
   )
 
   while [[ "$#" -gt 0 ]]
